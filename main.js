1e840b38e97b46be741da5abb383365cabbe69a5
Suman Malakar <malakars041@gmail.com>
2019-11-22 22:07:45 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// Sticky Navigation
+
+let navbar = $(".navbar");
+
+$(window).scroll(function(){
+
+    let oTop = $(".section-1").offset().top - window.innerHeight;
+    if($(window).scrollTop()>oTop){
+        navbar.addClass("sticky");
+    }
+    else{
+        navbar.removeClass("sticky");
+    }
+
+
+    // console.log(oTop);
+});
+
+
+// Counter Animation
+
+let nCount = function(selector){
+    $(selector).each(function(){
+        $(this).animate({
+            Counter:$(this).text()
+        },{
+            duration:4000,
+            easing:"swing",
+            step:function(value){
+                $(this).text(Math.ceil(value));
+            }
+        });
+    });
+};
+
+let a=0; 
+$(window).scroll(function(){
+      let oTop = $(".numbers").offset().top - window.innerHeight;
+     if(a==0&&$(window).scrollTop()>=oTop){
+         a++;
+         nCount(".rect>h1");
+     }
+});
