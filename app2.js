06f92a600c36d185cf5869a52fcf139520a3e96d
Suman Malakar <malakars041@gmail.com>
2019-11-23 16:23:50 +0530

    changing app.js , app2.js

---
+++ app2.js
@@ -0,0 +1,139 @@
+// Navbar
+
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+    // viewFactor: 0.2
+
+
+});
+
+// kamesh-img & text(typed.js)
+
+window.sr = ScrollReveal();
+sr.reveal('.kamesh', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+    // viewFactor: 0.2
+
+
+});
+
+// arrow-down
+
+window.sr = ScrollReveal();
+sr.reveal('.arrow-down', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+    // viewFactor: 0.2
+
+
+});
+
+
+// About section
+
+window.sr = ScrollReveal();
+sr.reveal('.left-img', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+
+
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.panel', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    viewFactor: 0.2
+
+
+});
+
+// pannel h1
+
+window.sr = ScrollReveal();
+sr.reveal('.heading', {
+    duration: 4000,
+    origin: 'top',
+    // distance: '300px',
+   
+});
+
+// pannel p
+
+window.sr = ScrollReveal();
+sr.reveal('.paragraph', {
+    duration: 5000,
+    origin: 'bootom',
+    // distance: '300px',
+  
+});
+
+//  section-2 heading
+
+window.sr = ScrollReveal();
+sr.reveal('.sub-heading', {
+    duration: 1500,
+    origin: 'top',
+    distance: '300px',
+  
+
+});
+
+//  counting js
+
+window.sr = ScrollReveal();
+sr.reveal('.numbers', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px',
+
+
+});
+
+// address
+window.sr = ScrollReveal();
+sr.reveal('.address', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+
+
+});
+
+// follow
+
+window.sr = ScrollReveal();
+sr.reveal('.follow', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+
+});
+
+// social media
+
+window.sr = ScrollReveal();
+sr.reveal('.social-button', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '200px',
+
+});
+
+// last 
+window.sr = ScrollReveal();
+sr.reveal('.last', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '200px',
+
+});
