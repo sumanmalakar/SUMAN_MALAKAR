ee4dda48e8102667b93a4ec15ca03ef48616e978
Suman Malakar <malakars041@gmail.com>
2019-11-24 16:37:08 +0530

    changing index.html

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 // Sticky Navigation
 
-let navbar = $(".navbar");
+let navbar = $(".navbarr");
 
 $(window).scroll(function(){
 
@@ -15,6 +16,42 @@ $(window).scroll(function(){
 
     // console.log(oTop);
 });
+
+
+// mobile navigation
+
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //  Animate links
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+   
+navSlide();
+
+
+
+
+
+
+
 
 
 // Counter Animation
