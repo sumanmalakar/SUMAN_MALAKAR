f0fa5b699ecba6342354291ba3dbfa3f2dd55581
Suman Malakar <malakars041@gmail.com>
2019-11-25 20:58:50 +0530

    changing app2.js , img folder

---
+++ js/main2.js
@@ -0,0 +1,44 @@
+// Sticky Navigation
+
+let navbar = $(".navbarr");
+
+$(window).scroll(function () {
+
+    let oTop = $(".section1").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky");
+    }
+
+
+    // console.log(oTop);
+});
+
+// mobile navigation
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //  Animate links
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
